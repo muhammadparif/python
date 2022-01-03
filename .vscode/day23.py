5c80f910912512396abe6e07008f30a6ab230f0a
muhammadparif <muhammadparif028@gmail.com>
2022-01-03 11:07:07 +0800

    menambahkan file day23.py

---
+++ .vscode/day23.py
@@ -0,0 +1,13 @@
+def main ():
+    print ("latihan fungsi")
+    print ("ini latihan fungsi saya")
+
+def menu2 ():
+    print("nama saya parif")
+
+i = 0
+while i < 10:
+    main()
+    menu2()
+    print ()
+    i += 1
