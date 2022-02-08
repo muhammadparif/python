a98be417c972f39f06c2174506b0079747a83b1d
muhammadparif <muhammadparif028@gmail.com>
2022-02-08 22:14:27 +0800

    belajar lambda

---
+++ .vscode/lambda1.py
@@ -0,0 +1,5 @@
+""" Diketahui suatu segitiga siku-siku memiliki alas dengan 
+panjang 12 cm dan tinggi 5 cm. Hitunglah luas segitiga tersebut!"""
+
+luas_segitiga=(lambda alas, tinggi: alas * tinggi/2)(12,5)
+print (f"jadi luas segitiga tersebut adalah {luas_segitiga} cm^2")
