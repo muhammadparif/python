cfcdcecaf4cf0e49ad280110ae51f576c15a4502
muhammadparif <muhammadparif028@gmail.com>
2022-02-17 22:09:35 +0800

    belajar class

---
+++ belajar_class.py
@@ -0,0 +1,9 @@
+class minuman :
+    nama = None
+    rasa = None
+minuman_1 = minuman()
+minuman_1.nama = "ale ale"
+minuman_1.rasa = "jeruk"
+print(f"saya punya minuman {minuman_1.nama} rasa {minuman_1.rasa}")
+
+# 
